b9dddd0147570140ace591bfdc8b5834bfa79182
Richard Lai <it.rclai@gmail.com>
2015-02-23 21:35:35 -0500

    Bumped to 0.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lai:collection-extensions',
-  version: '0.0.9',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Safely and easily extend the Mongo.Collection constructor with custom functionality.',
   // URL to the Git repository containing the source code for this package.
