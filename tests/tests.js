8e00c9c17758bb30c5587d54082a5922748bedbb
Jan Dvorak <storyteller@freedombase.net>
2023-01-31 21:31:09 +0900

    Fix bad test merge

--- tests/tests.js
+++ tests/tests.js
@@ -119,6 +119,14 @@ describe('tets', function () {
     assert.equal(todo.title, 'Pick up more stuff')
   })
 
+  it('inheritance - Shows the db-functions as properties of the prototype', function () {
+    const Todos = createCollection(randomName('todos'))
+    // Todos = new Mongo.Collection('todos' + test.id);
+    console.log(Object.keys(Mongo.Collection.prototype))
+    assert.instanceOf(Todos, Mongo.Collection)
+    assert.include(Object.keys(Mongo.Collection.prototype), 'update')
+  })
+
   it('instanceof - matches Mongo.Collection', function () {
     const collectionName = randomName('foo')
     const Test = createCollection(collectionName)
@@ -147,58 +155,18 @@ describe('tets', function () {
     const extension = function () {
       arr.push(1)
     }
-  });
+    CollectionExtensions.addExtension(extension)
+    createCollection(null)
+    assert.equal(arr[0], 1)
+    clearExtension(extension)
+  })
 
-  todo = inst(Todos);
-  test.equal(todo.title, 'Pick up more stuff');
-});
-
-Tinytest.add('inheritance - Shows the db-functions as properties of the prototype', function(test) {
-  Todos = new Mongo.Collection('todos' + test.id);
-  console.log(Object.keys(Mongo.Collection.prototype));
-  test.include(Object.keys(Mongo.Collection.prototype), 'update');
-});
-
-Tinytest.add('instanceof - matches Mongo.Collection', function(test) {
-  var collectionName = 'foo' + test.id;
-  var Test = new Mongo.Collection(collectionName);
-  test.instanceOf(Test, Mongo.Collection);
-});
-
-Tinytest.add('instanceof - Meteor.Collection matches Mongo.Collection', function(test) {
-  var collectionName = 'foo' + test.id;
-  var Test = new Meteor.Collection(collectionName);
-  test.instanceOf(Test, Mongo.Collection);
-});
-
-Tinytest.add('instanceof - Meteor.users matches (Mongo/Meteor).Collection', function(test) {
-  test.instanceOf(Meteor.users, Mongo.Collection);
-  test.instanceOf(Meteor.users, Meteor.Collection);
-});
-
-Tinytest.add('instanceof - Mongo.Collection === Mongo.Collection.prototype.constructor', function(test) {
-  test.equal(Mongo.Collection, Mongo.Collection.prototype.constructor);
-  test.equal(Meteor.Collection, Mongo.Collection.prototype.constructor);
-  test.equal(Meteor.Collection, Meteor.Collection.prototype.constructor);
-});
-
-Tinytest.add('functionality - Add a collection extension', function(test) {
-  var arr = [];
-  var extension = function() {
-    arr.push(1);
-  };
-  CollectionExtensions.addExtension(extension);
-  new Mongo.Collection(null);
-  test.equal(arr[0], 1);
-  clearExtension(extension);
-});
-
-Tinytest.add('functionality - Add a collection extension that adds initial documents', function(test) {
-  var arr = [];
-  var INITIAL_DOCUMENTS = 3;
-  var extension = function() {
-    for (var i = 0; i < INITIAL_DOCUMENTS; i++) {
-      this.insert({ a: i });
+  it('functionality - Add a collection extension that adds initial documents', function () {
+    const INITIAL_DOCUMENTS = 3
+    const extension = function () {
+      for (let i = 0; i < INITIAL_DOCUMENTS; i++) {
+        this.insert({ a: i })
+      }
     }
     CollectionExtensions.addExtension(extension)
     const testCollection = createCollection(null)
