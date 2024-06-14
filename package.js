4fa37f23400430c196da3be50151951f1887e8bc
jankapunkt <jkuester@uni-bremen.de>
2024-06-14 09:33:41 +0200

    published 1.0.0-rc300.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'lai:collection-extensions',
-  version: '1.0.0-rc300.0',
+  version: '1.0.0-rc300.1',
   // Brief, one-line summary of the package.
   summary: 'Safely and easily extend the Mongo.Collection constructor with custom functionality.',
   // URL to the Git repository containing the source code for this package.
@@ -14,7 +14,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.addFiles = api.addFiles || api.add_files // backwards-compat
-  api.versionsFrom(['2.8.0', '3.0-rc.2'])
+  api.versionsFrom(['2.8.0', '3.0-rc.4'])
 
   api.use([
     'ecmascript',
@@ -36,14 +36,14 @@ Package.onTest(function (api) {
     'underscore',
     'aldeed:collection2@3.0.0 || 4.0.0',
     'aldeed:simple-schema@2.0.0-rc300.0 || 1.13.1',
-    'meteortesting:mocha@3.1.0-rc.0',
-    'meteortesting:browser-tests@1.6.0-rc300.0',
+    'meteortesting:mocha@3.1.0-rc.1',
+    'meteortesting:browser-tests@1.6.0-rc300.1',
     // 'matb33:collection-hooks@1.4.0-beta300.0',
     // 'ongoworks:security@1.0.1',
     // 'cfs:standard-packages',
     // 'cfs:gridfs',
-    'dburles:mongo-collection-instances@1.0.0-rc300.0',
-    'lai:collection-extensions@1.0.0-beta300.2'
+    'dburles:mongo-collection-instances@1.0.0-rc300.1',
+    'lai:collection-extensions@1.0.0-rc300.1'
   ])
   api.addFiles([
     'tests/functions.js',
